a06dc0dc79776b5e33a115a25fc43d8df45b4d2f
XnipS <23164022+XnipS@users.noreply.github.com>
2022-11-29 01:16:46 +1100

    Inital Day 1

---
+++ NIP-Manager/Master.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace NIP_Manager
+{
+	partial class Master
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if(disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Master));
+			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.strike_title = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.button3 = new System.Windows.Forms.Button();
+			this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.progressBar2 = new System.Windows.Forms.ProgressBar();
+			this.button6 = new System.Windows.Forms.Button();
+			this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+			this.flowLayoutPanel1.SuspendLayout();
+			this.flowLayoutPanel2.SuspendLayout();
+			this.flowLayoutPanel3.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// flowLayoutPanel1
+			// 
+			this.flowLayoutPanel1.AutoSize = true;
+			this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.flowLayoutPanel1.Controls.Add(this.strike_title);
+			this.flowLayoutPanel1.Controls.Add(this.label1);
+			this.flowLayoutPanel1.Controls.Add(this.button1);
+			this.flowLayoutPanel1.Controls.Add(this.button2);
+			this.flowLayoutPanel1.Controls.Add(this.progressBar1);
+			this.flowLayoutPanel1.Controls.Add(this.button3);
+			this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(206, 190);
+			this.flowLayoutPanel1.TabIndex = 0;
+			// 
+			// strike_title
+			// 
+			this.strike_title.AutoSize = true;
+			this.strike_title.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.strike_title.Location = new System.Drawing.Point(3, 0);
+			this.strike_title.Name = "strike_title";
+			this.strike_title.Size = new System.Drawing.Size(150, 30);
+			this.strike_title.TabIndex = 0;
+			this.strike_title.Text = "Strike Warfare";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label1.Location = new System.Drawing.Point(3, 30);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(126, 21);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Status: Unknown";
+			// 
+			// button1
+			// 
+			this.button1.Enabled = false;
+			this.button1.Location = new System.Drawing.Point(3, 54);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(200, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Install";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Enabled = false;
+			this.button2.Location = new System.Drawing.Point(3, 83);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(200, 23);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "Repair";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(3, 112);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(200, 23);
+			this.progressBar1.TabIndex = 4;
+			// 
+			// button3
+			// 
+			this.button3.Enabled = false;
+			this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.button3.Location = new System.Drawing.Point(3, 141);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(200, 46);
+			this.button3.TabIndex = 5;
+			this.button3.Text = "Play";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// flowLayoutPanel2
+			// 
+			this.flowLayoutPanel2.AutoSize = true;
+			this.flowLayoutPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.flowLayoutPanel2.Controls.Add(this.label2);
+			this.flowLayoutPanel2.Controls.Add(this.label3);
+			this.flowLayoutPanel2.Controls.Add(this.button4);
+			this.flowLayoutPanel2.Controls.Add(this.button5);
+			this.flowLayoutPanel2.Controls.Add(this.progressBar2);
+			this.flowLayoutPanel2.Controls.Add(this.button6);
+			this.flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+			this.flowLayoutPanel2.Location = new System.Drawing.Point(215, 3);
+			this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+			this.flowLayoutPanel2.Size = new System.Drawing.Size(206, 190);
+			this.flowLayoutPanel2.TabIndex = 1;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label2.Location = new System.Drawing.Point(3, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(70, 30);
+			this.label2.TabIndex = 0;
+			this.label2.Text = "Oxide";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label3.Location = new System.Drawing.Point(3, 30);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(126, 21);
+			this.label3.TabIndex = 1;
+			this.label3.Text = "Status: Unknown";
+			// 
+			// button4
+			// 
+			this.button4.Enabled = false;
+			this.button4.Location = new System.Drawing.Point(3, 54);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(200, 23);
+			this.button4.TabIndex = 2;
+			this.button4.Text = "Install";
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this.button5.Enabled = false;
+			this.button5.Location = new System.Drawing.Point(3, 83);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(200, 23);
+			this.button5.TabIndex = 3;
+			this.button5.Text = "Repair";
+			this.button5.UseVisualStyleBackColor = true;
+			// 
+			// progressBar2
+			// 
+			this.progressBar2.Location = new System.Drawing.Point(3, 112);
+			this.progressBar2.Name = "progressBar2";
+			this.progressBar2.Size = new System.Drawing.Size(200, 23);
+			this.progressBar2.TabIndex = 4;
+			// 
+			// button6
+			// 
+			this.button6.Enabled = false;
+			this.button6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.button6.Location = new System.Drawing.Point(3, 141);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(200, 46);
+			this.button6.TabIndex = 5;
+			this.button6.Text = "Play";
+			this.button6.UseVisualStyleBackColor = true;
+			// 
+			// flowLayoutPanel3
+			// 
+			this.flowLayoutPanel3.AutoSize = true;
+			this.flowLayoutPanel3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.flowLayoutPanel3.Controls.Add(this.flowLayoutPanel1);
+			this.flowLayoutPanel3.Controls.Add(this.flowLayoutPanel2);
+			this.flowLayoutPanel3.Location = new System.Drawing.Point(12, 12);
+			this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+			this.flowLayoutPanel3.Size = new System.Drawing.Size(424, 196);
+			this.flowLayoutPanel3.TabIndex = 2;
+			// 
+			// Master
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
+			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.flowLayoutPanel3);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "Master";
+			this.Text = "NIP-Manager";
+			this.Load += new System.EventHandler(this.Master_Load);
+			this.flowLayoutPanel1.ResumeLayout(false);
+			this.flowLayoutPanel1.PerformLayout();
+			this.flowLayoutPanel2.ResumeLayout(false);
+			this.flowLayoutPanel2.PerformLayout();
+			this.flowLayoutPanel3.ResumeLayout(false);
+			this.flowLayoutPanel3.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private FlowLayoutPanel flowLayoutPanel1;
+		private Label strike_title;
+		private Label label1;
+		private Button button1;
+		private Button button2;
+		private ProgressBar progressBar1;
+		private Button button3;
+		private FlowLayoutPanel flowLayoutPanel2;
+		private Label label2;
+		private Label label3;
+		private Button button4;
+		private Button button5;
+		private ProgressBar progressBar2;
+		private Button button6;
+		private FlowLayoutPanel flowLayoutPanel3;
+	}
+}
---
+++ NIP-Manager/Master.cs
@@ -0,0 +1,15 @@
+namespace NIP_Manager
+{
+	public partial class Master : Form
+	{
+		public Master()
+		{
+			InitializeComponent();
+		}
+
+		private void Master_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
